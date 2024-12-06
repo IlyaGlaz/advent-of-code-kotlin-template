ed736fd76336bde46c33d1209d32422f9852ac0d
IlyaGlaz <glazterix@gmail.com>
2024-12-06 18:31:51 +0300

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,27 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun getListOfPairs(input: List<String>): List<Pair<Long, Long>> =
+        input.map { line -> line.split("""\s+""".toRegex()).let { it[0].toLong() to it[1].toLong() } }
+
+    fun part1(input: List<String>): Long {
+        val (left, right) = getListOfPairs(input).unzip()
+
+        val result = left.sorted().zip(right.sorted()).sumOf { abs(it.first - it.second) }
+
+        return result
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val (left, right) = getListOfPairs(input).unzip()
+
+        val ids = left.toSet()
+        val similarIdsCount = right.filter { ids.contains(it) }.groupingBy { it }.eachCount()
+        val result = similarIdsCount.map { it.key * it.value }.sum()
+
+        return result
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
