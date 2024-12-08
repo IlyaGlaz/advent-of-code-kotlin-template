b2a60180dd87b119f5595720f1827c7e5bcde8bc
IlyaGlaz <glazterix@gmail.com>
2024-12-08 10:51:51 +0300

    Day02

---
+++ src/day01/Day01.kt
@@ -0,0 +1,32 @@
+package day01
+
+import println
+import readInput
+import kotlin.math.abs
+
+fun main() {
+    fun part1(list: List<Pair<Long, Long>>): Long {
+        val (left, right) = list.unzip()
+
+        val result = left.sorted().zip(right.sorted()).sumOf { abs(it.first - it.second) }
+
+        return result
+    }
+
+    fun part2(list: List<Pair<Long, Long>>): Long {
+        val (left, right) = list.unzip()
+
+        val ids = left.toSet()
+        val similarIdsCount = right.filter { ids.contains(it) }.groupingBy { it }.eachCount()
+        val result = similarIdsCount.map { it.key * it.value }.sum()
+
+        return result
+    }
+
+    fun getListOfPairs(input: List<String>): List<Pair<Long, Long>> =
+        input.map { line -> line.split("""\s+""".toRegex()).let { it[0].toLong() to it[1].toLong() } }
+
+    val input = readInput("day01/Day01")
+    part1(getListOfPairs(input)).println()
+    part2(getListOfPairs(input)).println()
+}
---
+++ src/day02/Day02.kt
@@ -0,0 +1,34 @@
+package day02
+
+import println
+import readInput
+
+fun main() {
+    fun isSafe(levels: List<Int>): Boolean {
+        val difference = levels.zipWithNext { a, b -> a - b }
+
+        return difference.all { it in -3..3 }
+                && (difference.all { it > 0 } || difference.all { it < 0 })
+    }
+
+    fun part1(input: List<List<Int>>): Int =
+        input.count(::isSafe)
+
+
+    fun part2(input: List<List<Int>>): Int =
+        input.count { numbers ->
+            numbers.indices.any {
+                val skipped = numbers.toMutableList().apply { removeAt(it) }
+                isSafe(skipped)
+            }
+        }
+
+    fun listOfReports(input: List<String>): List<List<Int>> = input.map { line ->
+        line.split("""\s""".toRegex()).map { it.toInt() }
+    }
+
+    val input = readInput("day02/Day02")
+    part1(listOfReports(input)).println()
+    part2(listOfReports(input)).println()
+}
+
